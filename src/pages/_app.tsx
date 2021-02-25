ae84f8e6ae230affd482e6d21b1feb29a2968cca
Dgeison <paradg@gmail.com>
2021-02-24 23:26:26 -0300

    insert values and your useState in aplication app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import "../styles/global.css";
 
-import { ChallengesContext } from "../contexts/ChallengesContext";
-import { useState } from "react";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
+import React, { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-
-  const [level, setLevel] = useState(1)
-
-  function levelUp(){
-    setLevel(level +1)
-  }
   return (
-    <ChallengesContext.Provider value={{level, levelUp}}>
+    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesContext.Provider>
+    </ChallengesProvider>
   );
 }
 
